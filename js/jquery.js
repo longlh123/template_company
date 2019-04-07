11d4abb75ecc4de161df18a199241927a9b17bcf
LongPham <longlh112633@gmail.com>
2019-04-07 21:44:49 +0700

    update trang iindex và slide_1 lần 2

--- js/jquery.js
+++ js/jquery.js
@@ -2,12 +2,32 @@
 jQuery(document).ready(function($) {
 
 	$('#slide_1').children().each(function(index, value){
-	   		
+	   	
    		if($(this).attr('id') != "slide_1_1" && $(this).prop('tagName') != "A")
    		{
    			$(this).css('display', 'none');		
    		}	
     });
+
+	$('.idx_slide').on('click', function(){
+
+		var slide_selected = $(this).parent().prop('id').replace('breadcrumb_','');
+
+		$('#slide_1').children().each(function(index, value){
+	   		
+	   		if($(this).prop('id') != slide_selected && $(this).prop('tagName') != "A")
+	   		{
+	   			$(this).css('display', 'none');
+
+	   			if($(this).prop('id') !== 'slide_1_1')
+	   			{
+	   				$('#breadcrumb_' + $(this).prop('id')).remove();	
+	   			}		
+	   		}	
+	    });
+
+	    $('#' + slide_selected).show();
+	});
 
 	$('.next').on('click',function(){
 	    
@@ -15,6 +35,7 @@ jQuery(document).ready(function($) {
 	    var count_slide = 0;
 
 	    var $this = $(this).parent();
+	    var $cur_slide = 'undefined';
 
 	    $this.children().each(function(index, value){
 			
@@ -24,16 +45,28 @@ jQuery(document).ready(function($) {
 
 	    		if($(this).css('display') != "none")
 		    	{
+		    		$cur_slide = $(this);
 		    		id = parseInt($(this).prop('id').replace($this.prop('id') + "_", "")) + 1;
 		    		$(this).hide();
 		    	}	
 	    	}
 	    });
-	    
-	   	if(id > count_slide) id = 1;
+		
+		if($cur_slide.prop('id') !== "slide_1_1")
+		{
+			$('#breadcrumb_' + $cur_slide.prop('id')).remove();	
+		}
 
+	   	if(id > count_slide)
+	   	{
+	   		id = 1;	
+	   	} 
+	   	else
+	   	{
+	   		$('#breadcrumb').last().append('<li id="breadcrumb_' + $this.prop('id') + "_" + id + '"><a>Slide ' + id + '</a></li>');
+	   	}
+		
 	   	$("#" + $this.prop('id') + "_" + id).show();
-	   	
 	});
 
 	$('.prev').on('click',function(){
@@ -42,6 +75,7 @@ jQuery(document).ready(function($) {
 	    var count_slide = 0;
 
 	    var $this = $(this).parent();
+	    var $cur_slide = 'undefined';
 
 	    $this.children().each(function(index, value){
 			
@@ -51,13 +85,29 @@ jQuery(document).ready(function($) {
 
 	    		if($(this).css('display') != "none")
 		    	{
+		    		$cur_slide = $(this);
 		    		id = parseInt($(this).prop('id').replace($this.prop('id') + "_", "")) - 1;
 		    		$(this).hide();
 		    	}	
 	    	}
 	    });
 	    
-	   	if(id == 0) id = count_slide;
+	    if($cur_slide.prop('id') !== "slide_1_1")
+		{
+			$('#breadcrumb_' + $cur_slide.prop('id')).remove();	
+		}
+
+	   	if(id == 0)
+	   	{ 
+	   		id = 1;
+	   	}
+	   	else
+	   	{
+	   		if(id > 1)
+	   		{
+	   			$('#breadcrumb').last().append('<li id="breadcrumb_' + $this.prop('id') + "_" + id + '"><a>Slide ' + id + '</a></li>');	
+	   		}
+	   	}
 
 	   	$("#" + $this.prop('id') + "_" + id).show();
 	});
